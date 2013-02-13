3386f847a34b4bf00284eea41ccd40f295b10b06
Carlos Cardona <cgcardona@gmail.com>
2013-02-12 19:27:57 -0800

    stub rails app with underscore and ember-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberStatic::Application.config.secret_token = '0fd17f2aec04da8015adeca8ec79774bb9b28bfcf8101454517582d4ba785a1e0f281c81ed03ebd7fb98d7924f28841685e813ad07543b4c903e4e4ec49dde65'
